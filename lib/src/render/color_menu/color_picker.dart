9210018cfff98594dd91fecf61616d58734806cf
Mukund-Tandon <mukundtandon1211@gmail.com>
2023-04-06 16:00:50 +0530

    feat: Added Custom color option for text Font Color and background color using Hex values

--- lib/src/render/color_menu/color_picker.dart
+++ lib/src/render/color_menu/color_picker.dart
@@ -46,6 +46,25 @@ class ColorPicker extends StatefulWidget {
 }
 
 class _ColorPickerState extends State<ColorPicker> {
+  final TextEditingController _fontColorHexController = TextEditingController();
+  final TextEditingController _fontColorOpacityController =
+      TextEditingController();
+  final TextEditingController _backgroundColorHexController =
+      TextEditingController();
+  final TextEditingController _backgroundColorOpacityController =
+      TextEditingController();
+  initState() {
+    super.initState();
+    _fontColorHexController.text =
+        _extractColorHex(widget.selectedFontColorHex) ?? 'FFFFFF';
+    _fontColorOpacityController.text =
+        _convertHexToOpacity(widget.selectedFontColorHex) ?? '100';
+    _backgroundColorHexController.text =
+        _extractColorHex(widget.selectedBackgroundColorHex) ?? 'FFFFFF';
+    _backgroundColorOpacityController.text =
+        _convertHexToOpacity(widget.selectedBackgroundColorHex) ?? '0';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -74,15 +93,22 @@ class _ColorPickerState extends State<ColorPicker> {
               _buildHeader('font color'),
               // padding
               const SizedBox(height: 6),
+              _buildCustomColorItem(_ColorType.font, _fontColorHexController,
+                  _fontColorOpacityController),
               _buildColorItems(
                 _ColorType.font,
                 widget.fontColorOptions,
                 widget.selectedFontColorHex,
               ),
+
               // background color
               const SizedBox(height: 6),
               _buildHeader('background color'),
               const SizedBox(height: 6),
+              _buildCustomColorItem(
+                  _ColorType.background,
+                  _backgroundColorHexController,
+                  _backgroundColorOpacityController),
               _buildColorItems(
                 _ColorType.background,
                 widget.backgroundColorOptions,
@@ -164,5 +190,116 @@ class _ColorPickerState extends State<ColorPicker> {
         ),
       ),
     );
+  }
+
+  Widget _buildCustomColorItem(
+      _ColorType type,
+      TextEditingController colorController,
+      TextEditingController opacityController) {
+    return ExpansionTile(
+      tilePadding: const EdgeInsets.only(left: 0),
+      title: SizedBox(
+        height: 36,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(width: 6),
+            SizedBox.square(
+              dimension: 12,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(int.tryParse(_combineColorHexAndOpacity(
+                          colorController.text, opacityController.text)) ??
+                      0xFFFFFFFF),
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+            const SizedBox(width: 6),
+            Expanded(
+              child: Text(
+                'Custom Color',
+                style:
+                    TextStyle(fontSize: 12, color: widget.pickerItemTextColor),
+              ),
+            ),
+          ],
+        ),
+      ),
+      children: [
+        const SizedBox(height: 6),
+        _customColorDetailsTextField('Hex Color', colorController, type),
+        const SizedBox(height: 6),
+        _customColorDetailsTextField('Opacity', opacityController, type),
+      ],
+    );
+  }
+
+  Widget _customColorDetailsTextField(
+      String labeText, TextEditingController controller, _ColorType? type) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labeText,
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+          ),
+        ),
+      ),
+      style: Theme.of(context).textTheme.bodyMedium,
+      onSubmitted: (value) {
+        if (type == _ColorType.font) {
+          final String color = _combineColorHexAndOpacity(
+            _fontColorHexController.text,
+            _fontColorOpacityController.text,
+          );
+          widget.onSubmittedFontColorHex(color);
+        } else if (type == _ColorType.background) {
+          final String color = _combineColorHexAndOpacity(
+            _backgroundColorHexController.text,
+            _backgroundColorOpacityController.text,
+          );
+          widget.onSubmittedbackgroundColorHex(color);
+        }
+      },
+    );
+  }
+
+  String _combineColorHexAndOpacity(String colorHex, String opacity) {
+    colorHex = _fixColorHex(colorHex);
+    opacity = _fixOpacity(opacity);
+    final opacityHex = (int.parse(opacity) * 2.55).round().toRadixString(16);
+    return '0x$opacityHex$colorHex';
+  }
+
+  String _fixColorHex(String colorHex) {
+    if (colorHex.length > 6) {
+      return colorHex.substring(0, 6);
+    }
+    return colorHex;
+  }
+
+  String _fixOpacity(String opacity) {
+    RegExp regex = RegExp(r'[a-zA-Z]');
+    if (regex.hasMatch(opacity) ||
+        int.parse(opacity) > 100 ||
+        int.parse(opacity) < 0) {
+      return '100';
+    }
+    return opacity;
+  }
+
+  String? _convertHexToOpacity(String? colorHex) {
+    if (colorHex == null) return null;
+    final opacityHex = colorHex.substring(2, 4);
+    final opacity = int.parse(opacityHex, radix: 16) / 2.55;
+    return opacity.toStringAsFixed(0);
+  }
+
+  String? _extractColorHex(String? colorHex) {
+    if (colorHex == null) return null;
+    return '${colorHex.substring(4)}';
   }
 }
