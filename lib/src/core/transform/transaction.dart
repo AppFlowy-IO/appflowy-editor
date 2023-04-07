c690d2eceb5e179c9da5867498b63ba22b3dc0df
squidrye <mihirs167@gmail.com>
2023-04-07 16:26:38 +0530

    refactor: revert inSelection, is not able to differentiate between overlays and selection.

--- lib/src/core/transform/transaction.dart
+++ lib/src/core/transform/transaction.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:appflowy_editor/src/extensions/node_extensions.dart';
 import 'package:appflowy_editor/src/core/document/attributes.dart';
 import 'package:appflowy_editor/src/core/document/document.dart';
 import 'package:appflowy_editor/src/core/document/node.dart';
@@ -69,7 +68,7 @@ class Transaction {
   /// Deletes the [Node] in the document.
   void deleteNode(Node node) {
     deleteNodesAtPath(node.path);
-    if (beforeSelection != null && !node.inSelection(beforeSelection!)) {
+    if (beforeSelection != null && !node.path.equals(beforeSelection!.start.path)) {
       afterSelection = beforeSelection;
     }
   }
