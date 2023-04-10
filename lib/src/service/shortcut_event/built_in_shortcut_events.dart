ef9eb58d1a921c5706941f55a99265b51ac790bc
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-04-10 22:02:32 +0530

    feat: add find menu shortcut event

--- lib/src/service/shortcut_event/built_in_shortcut_events.dart
+++ lib/src/service/shortcut_event/built_in_shortcut_events.dart
@@ -4,6 +4,7 @@ import 'package:appflowy_editor/src/service/internal_key_event_handlers/copy_pas
 import 'package:appflowy_editor/src/service/internal_key_event_handlers/cursor_left_delete_handler.dart';
 import 'package:appflowy_editor/src/service/internal_key_event_handlers/enter_without_shift_in_text_node_handler.dart';
 import 'package:appflowy_editor/src/service/internal_key_event_handlers/exit_editing_mode_handler.dart';
+import 'package:appflowy_editor/src/service/internal_key_event_handlers/find_replace_handler.dart';
 import 'package:appflowy_editor/src/service/internal_key_event_handlers/markdown_syntax_to_styled_text.dart';
 import 'package:appflowy_editor/src/service/internal_key_event_handlers/outdent_handler.dart';
 import 'package:appflowy_editor/src/service/internal_key_event_handlers/page_up_down_handler.dart';
@@ -258,6 +259,13 @@ List<ShortcutEvent> builtInShortcutEvents = [
     key: 'selection menu',
     character: '/',
     handler: slashShortcutHandler,
+  ),
+  ShortcutEvent(
+    key: 'Find',
+    command: 'meta+f',
+    windowsCommand: 'ctrl+f',
+    linuxCommand: 'ctrl+f',
+    handler: findShortcutHandler,
   ),
   ShortcutEvent(
     key: 'enter',
