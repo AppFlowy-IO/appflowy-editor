4fdfa2e7ef4502b0211faeb1c0e3e4fdfcf0d4b0
alihassan143 <alihassan143cool@gmail.com>
2023-04-11 16:53:07 +0500

    test added

--- test/infra/html_converter_test.dart
+++ test/infra/html_converter_test.dart
@@ -10,6 +10,14 @@ void main() {
       expect(nodes.isNotEmpty, true);
     });
   });
+  group('HTMLConverterDocument tests', () {
+    test('HTMLToNodesConverter', () {
+      final converter = HTMLToNodesConverter(rawHTML);
+      final document = converter.toDocument();
+
+      expect(document.root.children.isNotEmpty, true);
+    });
+  });
 }
 
 const rawHTML = """<h1>AppFlowyEditor</h1>
