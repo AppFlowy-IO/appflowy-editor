ecba7ba49ba53d7c18bc1bf6cbc202bacaf761bd
Mathias Mogensen <42929161+Xazin@users.noreply.github.com>
2023-04-14 04:10:43 +0200

    fix: lessen horizontal editor padding for mobile (#70)
    * fix: lessen horizontal editor padding for mobile
    
    * Update lib/src/render/style/editor_style.dart
    
    Co-authored-by: Mathias Mogensen <42929161+Xazin@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lucas.Xu <lucas.xu@appflowy.io>

--- lib/src/render/style/editor_style.dart
+++ lib/src/render/style/editor_style.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 Iterable<ThemeExtension<dynamic>> get lightEditorStyleExtension => [
@@ -189,7 +191,9 @@ class EditorStyle extends ThemeExtension<EditorStyle> {
   }
 
   static final light = EditorStyle(
-    padding: const EdgeInsets.fromLTRB(200.0, 0.0, 200.0, 0.0),
+    padding: Platform.isAndroid || Platform.isIOS
+        ? const EdgeInsets.symmetric(horizontal: 20)
+        : const EdgeInsets.symmetric(horizontal: 200),
     backgroundColor: Colors.white,
     cursorColor: const Color(0xFF00BCF0),
     selectionColor: const Color.fromARGB(53, 111, 201, 231),
