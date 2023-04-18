6bab6494b86c97b27033d33934b0fe9459474ddb
Lucas.Xu <lucas.xu@appflowy.io>
2023-04-18 16:12:45 +0800

    feat: implement bulleted list component

--- lib/src/block_component/block_component.dart
+++ lib/src/block_component/block_component.dart
@@ -3,3 +3,6 @@ export 'text_block_component/text_block_component.dart';
 
 // to-do list
 export 'todo_list_block_component/todo_list_block_component.dart';
+
+// bulleted list
+export 'bulleted_list_component/bulleted_list_component.dart';
