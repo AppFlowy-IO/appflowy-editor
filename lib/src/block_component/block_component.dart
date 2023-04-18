d12f3a3bccb10b9bc54f4f766755256abefd1912
Lucas.Xu <lucas.xu@appflowy.io>
2023-04-18 16:22:55 +0800

    feat: implement numbered list component

--- lib/src/block_component/block_component.dart
+++ lib/src/block_component/block_component.dart
@@ -5,4 +5,7 @@ export 'text_block_component/text_block_component.dart';
 export 'todo_list_block_component/todo_list_block_component.dart';
 
 // bulleted list
-export 'bulleted_list_component/bulleted_list_component.dart';
+export 'bulleted_list_block_component/bulleted_list_block_component.dart';
+
+// numbered list
+export 'numbered_list_block_component/numbered_list_block_component.dart';
