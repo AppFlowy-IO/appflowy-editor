47d5fe68d11a06eb925a331222354eb502dfab47
Mayur Mahajan <mayur23smahajan@gmail.com>
2023-04-19 11:30:37 +0530

    chore: remove unnecessary import

--- lib/src/service/internal_key_event_handlers/outdent_handler.dart
+++ lib/src/service/internal_key_event_handlers/outdent_handler.dart
@@ -1,5 +1,4 @@
 import 'package:appflowy_editor/appflowy_editor.dart';
-import 'package:appflowy_editor/src/extensions/text_node_extensions.dart';
 import 'package:flutter/material.dart';
 
 ShortcutEventHandler outdentTabHandler = (editorState, event) {
