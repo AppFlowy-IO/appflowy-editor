1e9332886204082ca67fb98e6f1862d862839204
Lucas.Xu <lucas.xu@appflowy.io>
2023-04-25 22:24:40 +0800

    chore: export delta_builder in util.dart

---
+++ test/new/util/delta_builder_util.dart
@@ -0,0 +1,3 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
+
+typedef DeltaBuilder = Delta Function(int index);
--- test/new/util/util.dart
+++ test/new/util/util.dart
@@ -1,3 +1,4 @@
 export 'document_util.dart';
 export 'node_util.dart';
 export 'editor_state_util.dart';
+export 'delta_builder_util.dart';
