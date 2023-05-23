bb578ebc2142e59a9461ee8670d58ac6d90a9afb
Lucas.Xu <lucas.xu@appflowy.io>
2023-05-24 00:09:13 +0800

    fix: the selection won't update if the value is same

--- lib/src/editor/util/property_notifier.dart
+++ lib/src/editor/util/property_notifier.dart
@@ -1,15 +1,27 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 /// [PropertyValueNotifier] is a subclass of [ValueNotifier].
 ///
 /// The difference is that [PropertyValueNotifier] will notify listeners even
 /// when the value is the same as the previous value.
-class PropertyValueNotifier<T> extends ValueNotifier<T> {
-  PropertyValueNotifier(T value) : super(value);
+///
+///
 
+class PropertyValueNotifier<T> extends ChangeNotifier
+    implements ValueListenable<T> {
+  /// Creates a [ChangeNotifier] that wraps this value.
+  PropertyValueNotifier(this._value);
+
+  /// The current value stored in this notifier.
+  ///
+  /// When the value is replaced with something that is not equal to the old
+  /// value as evaluated by the equality operator ==, this class notifies its
+  /// listeners.
   @override
-  // ignore: unnecessary_overrides
-  void notifyListeners() {
-    super.notifyListeners();
+  T get value => _value;
+  T _value;
+  set value(T newValue) {
+    _value = newValue;
+    notifyListeners();
   }
 }
