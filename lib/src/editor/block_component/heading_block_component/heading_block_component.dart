72a9f2ce3b197718c6e4ec98467b05dee2c4de1b
Lucas.Xu <lucas.xu@appflowy.io>
2023-05-24 09:32:49 +0800

    fix: flutter analyze

--- lib/src/editor/block_component/heading_block_component/heading_block_component.dart
+++ lib/src/editor/block_component/heading_block_component/heading_block_component.dart
@@ -1,5 +1,4 @@
 import 'package:appflowy_editor/appflowy_editor.dart';
-import 'package:appflowy_editor/src/editor/block_component/base_component/block_component_action_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:collection/collection.dart';
