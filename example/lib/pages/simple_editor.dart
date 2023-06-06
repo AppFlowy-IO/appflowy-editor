1c9dbe8ce101b56be374437807b129b98eb85398
Yijing Huang <hyj891204@gmail.com>
2023-06-06 13:58:15 -0500

    feat: add heading menu

--- example/lib/pages/simple_editor.dart
+++ example/lib/pages/simple_editor.dart
@@ -69,7 +69,7 @@ class SimpleEditor extends StatelessWidget {
                   editorState: editorState,
                   toolbarItems: [
                     textDecorationMobileToolbarItem,
-                    // headingMobileToolbarItem,
+                    headingMobileToolbarItem,
                     // listMobileToolbarItem,
                     linkMobileToolbarItem,
                     // quoteMobileToolbarItem,
--- lib/src/editor/toolbar/mobile/toolbar_items/heading_mobile_toolbar_item.dart
+++ lib/src/editor/toolbar/mobile/toolbar_items/heading_mobile_toolbar_item.dart
@@ -4,19 +4,89 @@ import 'package:flutter/material.dart';
 final headingMobileToolbarItem = MobileToolbarItem.withMenu(
   itemIcon: const AFMobileIcon(afMobileIcons: AFMobileIcons.h1),
   itemMenuBuilder: (editorState, selection) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 8,
-        childAspectRatio: 5,
-      ),
-      children: const [
-        // TODO(yijing): Implement the heading toolbar item.
-        // heading 1
-        // heading 2
-        // heading 3
-      ],
+    return _HeadingMenu(
+      selection,
+      editorState,
     );
   },
 );
+
+class _HeadingMenu extends StatefulWidget {
+  const _HeadingMenu(
+    this.selection,
+    this.editorState, {
+    Key? key,
+  }) : super(key: key);
+
+  final Selection selection;
+  final EditorState editorState;
+
+  @override
+  State<_HeadingMenu> createState() => _HeadingMenuState();
+}
+
+class _HeadingMenuState extends State<_HeadingMenu> {
+  final headings = [
+    {
+      'icon': AFMobileIcons.h1,
+      'label': 'Heading 1',
+      'level': 1,
+    },
+    {
+      'icon': AFMobileIcons.h2,
+      'label': 'Heading 2',
+      'level': 2,
+    },
+    {
+      'icon': AFMobileIcons.h3,
+      'label': 'Heading 3',
+      'level': 3,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final btnList = headings.map((e) {
+      final icon = e['icon'] as AFMobileIcons;
+      final label = e['label'] as String;
+      final level = e['level'] as int;
+
+      // Check if current node is heading and its level
+      final node =
+          widget.editorState.getNodeAtPath(widget.selection.start.path)!;
+      final isSelected =
+          node.type == 'heading' && node.attributes['level'] == level;
+
+      return MobileToolbarItemMenuBtn(
+        icon: AFMobileIcon(afMobileIcons: icon),
+        label: label,
+        isSelected: isSelected,
+        onPressed: () {
+          setState(() {
+            widget.editorState.formatNode(
+              widget.selection,
+              (node) => node.copyWith(
+                type: isSelected
+                    ? ParagraphBlockKeys.type
+                    : HeadingBlockKeys.type,
+                attributes: {
+                  HeadingBlockKeys.level: level,
+                  HeadingBlockKeys.backgroundColor:
+                      node.attributes[blockComponentBackgroundColor],
+                  'delta': (node.delta ?? Delta()).toJson(),
+                },
+              ),
+            );
+          });
+        },
+      );
+    }).toList();
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ...btnList,
+      ],
+    );
+  }
+}
