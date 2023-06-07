dfc49184b8c1df50f76f6ed224b41df44855ed29
Gunjan Lunkad <67200542+glunkad@users.noreply.github.com>
2023-06-07 13:28:28 +0530

    feat: added divider to work with * (#118)
    * feat: Added divider to work with *
    
    * chore: Resolved the suggested changes
    
    * refactor: removed the check for star character
    
    * feat:added  character variable to insert divider .
    
    * feat: support converting stars ** to divider
    
    ---------
    
    Co-authored-by: Lucas.Xu <lucas.xu@appflowy.io>

---
+++ lib/src/editor/block_component/divider_block_component/divider_menu_item.dart
@@ -0,0 +1,31 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:appflowy_editor/src/render/selection_menu/selection_menu_icon.dart';
+import 'package:flutter/material.dart';
+
+SelectionMenuItem dividerMenuItem = SelectionMenuItem(
+  name: 'Divider',
+  icon: (editorState, isSelected, style) => SelectionMenuIconWidget(
+    icon: Icons.horizontal_rule,
+    isSelected: isSelected,
+    style: style,
+  ),
+  keywords: ['horizontal rule', 'divider'],
+  handler: (editorState, _, __) {
+    final selection = editorState.selection;
+    if (selection == null || !selection.isCollapsed) {
+      return;
+    }
+    final path = selection.end.path;
+    final node = editorState.getNodeAtPath(path);
+    final delta = node?.delta;
+    if (node == null || delta == null) {
+      return;
+    }
+    final insertedPath = delta.isEmpty ? path : path.next;
+    final transaction = editorState.transaction
+      ..insertNode(insertedPath, dividerNode())
+      ..insertNode(insertedPath, paragraphNode())
+      ..afterSelection = Selection.collapse(insertedPath.next, 0);
+    editorState.apply(transaction);
+  },
+);
