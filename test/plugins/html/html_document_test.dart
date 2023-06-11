89ee8fbec877079c493ede49d4d465d26d02d338
Ali Hassan <71745182+alihassan143@users.noreply.github.com>
2023-06-11 11:21:21 +0500

    fix: nested elements getting ignored (#178)
    * fix: nested elements getting ignored
    
    * fix: quote block return single node
    
    * fix:quote block element return quotenode

--- test/plugins/html/html_document_test.dart
+++ test/plugins/html/html_document_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:appflowy_editor/appflowy_editor.dart';
+import 'dart:developer';
 
 void main() {
   group('html_document_test.dart tests', () {
@@ -7,11 +8,21 @@ void main() {
       final document = htmlToDocument(rawHTML);
       expect(document.toJson(), data);
     });
+    test('nestedhtmlToDocument()', () {
+      final document = htmlToDocument(nestedhtml);
+      expect(document.toJson(), nestedDelta);
+    });
   });
   group('document_html_test.dart tests', () {
     test('documentToHtml()', () {
       final document = documentToHTML(Document.fromJson(data));
+
       expect(document, rawHTML);
+    });
+    test('nesteddocumentToHtml()', () {
+      final document = documentToHTML(Document.fromJson(nestedDelta));
+
+      expect(document, nestedhtml);
     });
   });
 }
@@ -287,4 +298,197 @@ const data = {
   }
 };
 const rawHTML =
-    '''<h1>AppFlowyEditor</h1><h2>👋 <strong>Welcome to</strong>   <span style="font-weight: bold; font-style: italic">AppFlowy Editor</span></h2><p>AppFlowy Editor is a <strong>highly customizable</strong>   <i>rich-text editor</i></p><p>   <u>Here</u> is an example <del>your</del> you can give a try</p><p>   <span style="font-weight: bold; font-style: italic">Span element</span></p><p>   <u>Span element two</u></p><p>   <span style="font-weight: bold; text-decoration: line-through">Span element three</span></p><p>   <a href="https://appflowy.io">This is an anchor tag!</a></p><h3>Features!</h3><ul><li><p>[x] Customizable</p></li><li><p>[x] Test-covered</p></li><li><p>[ ] more to come!</p></li><li><p>First item</p></li><li><p>Second item</p></li><li><p>List element</p></li></ul><blockquote><p>This is a quote!</p></blockquote><p><code> Code block</code></p><p>   <i>Italic one</i></p><p>   <i>Italic two</i></p><p>   <strong>Bold tag</strong></p><p>You can also use <span style="font-weight: bold; font-style: italic">AppFlowy Editor</span> as a component to build your own app. </p><h3>Awesome features</h3><p>If you have questions or feedback, please submit an issue on Github or join the community along with 1000+ builders!</p><p></p><p></p>''';
+    '''<h1>AppFlowyEditor</h1><h2>👋 <strong>Welcome to</strong>   <span style="font-weight: bold; font-style: italic">AppFlowy Editor</span></h2><p>AppFlowy Editor is a <strong>highly customizable</strong>   <i>rich-text editor</i></p><p>   <u>Here</u> is an example <del>your</del> you can give a try</p><p>   <span style="font-weight: bold; font-style: italic">Span element</span></p><p>   <u>Span element two</u></p><p>   <span style="font-weight: bold; text-decoration: line-through">Span element three</span></p><p>   <a href="https://appflowy.io">This is an anchor tag!</a></p><h3>Features!</h3><ul><li>[x] Customizable</li><li>[x] Test-covered</li><li>[ ] more to come!</li><li>First item</li><li>Second item</li><li>List element</li></ul><blockquote>This is a quote!</blockquote><p><code> Code block</code></p><p>   <i>Italic one</i></p><p>   <i>Italic two</i></p><p>   <strong>Bold tag</strong></p><p>You can also use <span style="font-weight: bold; font-style: italic">AppFlowy Editor</span> as a component to build your own app. </p><h3>Awesome features</h3><p>If you have questions or feedback, please submit an issue on Github or join the community along with 1000+ builders!</p><p></p><p></p>''';
+
+const nestedhtml =
+    '''<h1>Welcome to the playground</h1><blockquote>In case you were wondering what the black box at the bottom is – it\'s the debug view, showing the current state of the editor. You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting. The playground is a demo environment built with <code>@lexical/react</code>. Try typing in <strong>some text</strong> with <i>different</i> formats.</blockquote><p>\t</p><img src="https://richtexteditor.com/images/editor-image.png"><p>Make sure to check out the various plugins in the toolbar. You can also use #hashtags or @-mentions too!</p><p></p><p>If you\'d like to find out more about Lexical, you can:</p><ul><li>Visit the <a href="https://lexical.dev/">Lexical website</a> for documentation and more information.</li><li>\t<span><img src="https://richtexteditor.com/images/editor-image.png"></span></li><li>Check out the code on our <a href="https://github.com/facebook/lexical">GitHub repository</a>.</li><li>Playground code can be found <a href="https://github.com/facebook/lexical/tree/main/packages/lexical-playground">here</a>.</li><li>Join our <a href="https://discord.com/invite/KmG4wQnnD9">Discord Server</a> and chat with the team.</li><li>Playground code can be found <a href="https://github.com/facebook/lexical/tree/main/packages/lexical-playground">here</a>.</li></ul><p>Lastly, we\'re constantly adding cool new features to this playground. So make sure you check back here when you next get a chance 🙂.</p><p></p>''';
+const nestedDelta = {
+  'document': {
+    'type': 'page',
+    'children': [
+      {
+        'type': 'heading',
+        'data': {
+          'level': 1,
+          'delta': [
+            {'insert': 'Welcome to the playground'}
+          ]
+        }
+      },
+      {
+        'type': 'quote',
+        'data': {
+          'delta': [
+            {
+              'insert':
+                  'In case you were wondering what the black box at the bottom is – it\'s the debug view, showing the current state of the editor. You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting. The playground is a demo environment built with '
+            },
+            {
+              'insert': '@lexical/react',
+              'attributes': {'code': true}
+            },
+            {'insert': '. Try typing in '},
+            {
+              'insert': 'some text',
+              'attributes': {'bold': true}
+            },
+            {'insert': ' with '},
+            {
+              'insert': 'different',
+              'attributes': {'italic': true}
+            },
+            {'insert': ' formats.'}
+          ]
+        }
+      },
+      {
+        'type': 'paragraph',
+        'data': {
+          'delta': [
+            {'insert': '\t'}
+          ]
+        }
+      },
+      {
+        'type': 'image',
+        'data': {
+          'url': 'https://richtexteditor.com/images/editor-image.png',
+          'align': 'center',
+          'height': null,
+          'width': null
+        }
+      },
+      {
+        'type': 'paragraph',
+        'data': {
+          'delta': [
+            {
+              'insert':
+                  'Make sure to check out the various plugins in the toolbar. You can also use #hashtags or @-mentions too!'
+            }
+          ]
+        }
+      },
+      {
+        'type': 'paragraph',
+        'data': {'delta': []}
+      },
+      {
+        'type': 'paragraph',
+        'data': {
+          'delta': [
+            {
+              'insert':
+                  'If you\'d like to find out more about Lexical, you can:'
+            }
+          ]
+        }
+      },
+      {
+        'type': 'bulleted_list',
+        'data': {
+          'delta': [
+            {'insert': 'Visit the '},
+            {
+              'insert': 'Lexical website',
+              'attributes': {'href': 'https://lexical.dev/'}
+            },
+            {'insert': ' for documentation and more information.'}
+          ]
+        }
+      },
+      {
+        'type': 'bulleted_list',
+        'children': [
+          {
+            'type': 'image',
+            'data': {
+              'url': 'https://richtexteditor.com/images/editor-image.png',
+              'align': 'center',
+              'height': null,
+              'width': null
+            }
+          }
+        ],
+        'data': {
+          'delta': [
+            {'insert': '\t'}
+          ]
+        }
+      },
+      {
+        'type': 'bulleted_list',
+        'data': {
+          'delta': [
+            {'insert': 'Check out the code on our '},
+            {
+              'insert': 'GitHub repository',
+              'attributes': {'href': 'https://github.com/facebook/lexical'}
+            },
+            {'insert': '.'}
+          ]
+        }
+      },
+      {
+        'type': 'bulleted_list',
+        'data': {
+          'delta': [
+            {'insert': 'Playground code can be found '},
+            {
+              'insert': 'here',
+              'attributes': {
+                'href':
+                    'https://github.com/facebook/lexical/tree/main/packages/lexical-playground'
+              }
+            },
+            {'insert': '.'}
+          ]
+        }
+      },
+      {
+        'type': 'bulleted_list',
+        'data': {
+          'delta': [
+            {'insert': 'Join our '},
+            {
+              'insert': 'Discord Server',
+              'attributes': {'href': 'https://discord.com/invite/KmG4wQnnD9'}
+            },
+            {'insert': ' and chat with the team.'}
+          ]
+        }
+      },
+      {
+        'type': 'bulleted_list',
+        'data': {
+          'delta': [
+            {'insert': 'Playground code can be found '},
+            {
+              'insert': 'here',
+              'attributes': {
+                'href':
+                    'https://github.com/facebook/lexical/tree/main/packages/lexical-playground'
+              }
+            },
+            {'insert': '.'}
+          ]
+        }
+      },
+      {
+        'type': 'paragraph',
+        'data': {
+          'delta': [
+            {
+              'insert':
+                  'Lastly, we\'re constantly adding cool new features to this playground. So make sure you check back here when you next get a chance 🙂.'
+            }
+          ]
+        }
+      },
+      {
+        'type': 'paragraph',
+        'data': {'delta': []}
+      }
+    ]
+  }
+};
