da5137a187bb0939d3ddd97d514c37aa57823efe
Yijing Huang <hyj891204@gmail.com>
2023-06-12 14:33:02 -0500

    chore: move toolbar/items to toolbar/desktop/items

--- lib/src/editor/toolbar/desktop/items/color/color_picker.dart
+++ lib/src/editor/toolbar/desktop/items/color/color_picker.dart
@@ -1,5 +1,5 @@
 import 'package:appflowy_editor/appflowy_editor.dart';
-import 'package:appflowy_editor/src/editor/toolbar/items/utils/overlay_util.dart';
+import 'package:appflowy_editor/src/editor/toolbar/desktop/items/utils/overlay_util.dart';
 import 'package:flutter/material.dart';
 
 class ColorOption {
