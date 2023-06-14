4a12008041dbe0c4b761e389592b303c5fd1569d
Mukund-Tandon <mukundtandon1211@gmail.com>
2023-06-14 13:03:10 +0530

    feat: added support for markdown codeblock to be converted to ducument

--- lib/src/plugins/markdown/decoder/document_markdown_decoder.dart
+++ lib/src/plugins/markdown/decoder/document_markdown_decoder.dart
@@ -7,9 +7,44 @@ class DocumentMarkdownDecoder extends Converter<String, Document> {
   Document convert(String input) {
     final lines = input.split('\n');
     final document = Document.blank();
+    //previously
+    // for (var i = 0; i < lines.length; i++) {
+    //   document.insert([i], [_convertLineToNode(lines[i])]);
+    // }
+    int i = 0;
+    //In the below while loop we iterate through each line of the input markdown
+    //if the line starts with ``` it means there are chances it may be a code block
+    // so it iterates through all the lines(using another while loop) storing them in a
+    //string variable codeblock until it fing another ``` which marks the end of code
+    // block it then sends this to convert _convertLineToNode function .
+    // If we reach the end of lines and we still haven't found ``` thenwe will treat the
+    // line starting with ``` as a regular paragraph and continue the main while loop from
+    // a temporary pointer which stored the line number of the line with starting ```
 
-    for (var i = 0; i < lines.length; i++) {
-      document.insert([i], [_convertLineToNode(lines[i])]);
+    while (i < lines.length) {
+      if (lines[i].startsWith("```")) {
+        String codeBlock = "";
+        codeBlock += "${lines[i]}\n";
+        int tempLinePointer = i;
+        i++;
+        while (!lines[i].endsWith("```") && i < lines.length) {
+          codeBlock += lines[i];
+          i++;
+        }
+
+        if (i == lines.length) {
+          document.insert([i], [_convertLineToNode(lines[i])]);
+          i = tempLinePointer;
+          i++;
+        } else {
+          codeBlock += lines[i];
+          document.insert([tempLinePointer], [_convertLineToNode(codeBlock)]);
+          i++;
+        }
+      } else {
+        document.insert([i], [_convertLineToNode(lines[i])]);
+        i++;
+      }
     }
 
     return document;
@@ -54,6 +89,8 @@ class DocumentMarkdownDecoder extends Converter<String, Document> {
       );
     } else if (line.isNotEmpty && RegExp('^-*').stringMatch(line) == line) {
       return Node(type: 'divider');
+    } else if (line.startsWith('```') && line.endsWith('```')){
+      return _codeBlockNodeFromMarkdown(line, decoder);
     }
 
     if (line.isNotEmpty) {
@@ -62,8 +99,33 @@ class DocumentMarkdownDecoder extends Converter<String, Document> {
       );
     }
 
+
     return paragraphNode(
       attributes: {'delta': Delta().toJson()},
     );
+  }
+
+  Node _codeBlockNodeFromMarkdown(
+      String markdown, DeltaMarkdownDecoder decoder) {
+    String codeStartMarker = "```";
+    String codeEndMarker = "```";
+    String language = "";
+    String codeContent = "";
+    int codeStartIndex = markdown.indexOf(codeStartMarker);
+    int codeEndIndex = markdown.indexOf(
+        codeEndMarker, codeStartIndex + codeStartMarker.length);
+
+    String codeBlock = markdown.substring(
+        codeStartIndex + codeStartMarker.length, codeEndIndex);
+    List<String> codeLines = codeBlock.trim().split('\n');
+
+    language = codeLines[0].trim();
+    List<String> codeContentLines = codeLines.sublist(1);
+    codeContent = codeContentLines.join('\n');
+
+    return Node(type: 'code', attributes: {
+      'delta': decoder.convert(codeContent).toJson(),
+      'language': language
+    });
   }
 }
