da79e9a131764437d4ec81d65cd6d7d9b05160c0
Vedant Pandey <vedantpandey46@gmail.com>
2023-06-27 08:54:12 +0530

    Add nested parsing and update test

--- lib/src/plugins/markdown/decoder/custom_syntaxes/underline_syntax.dart
+++ lib/src/plugins/markdown/decoder/custom_syntaxes/underline_syntax.dart
@@ -1,12 +1,13 @@
 import 'package:markdown/markdown.dart' as md;
 
 class UnderlineInlineSyntax extends md.InlineSyntax {
-  UnderlineInlineSyntax() : super('<u>(.*)<\/u>');
+  UnderlineInlineSyntax() : super('<u>(.*)</u>');
 
   @override
   bool onMatch(md.InlineParser parser, Match match) {
     final text = parser.source.substring(match.start + 3, match.end - 4);
-    parser.addNode(md.Element('u', [md.Text(text)]));
+    List<md.Node> nestedNodes = md.InlineParser(text, parser.document).parse();
+    parser.addNode(md.Element('u', nestedNodes));
     return true;
   }
 }
