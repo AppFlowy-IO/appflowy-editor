4f41e763ebebebcd8482ec4d5a03f1e7c0616ab8
Lucas.Xu <lucas.xu@appflowy.io>
2023-07-12 14:28:10 +0800

    test: add test

---
+++ test/customer/custom_toolbar_item_color_test.dart
@@ -0,0 +1,78 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() async {
+  setUpAll(() {
+    TestWidgetsFlutterBinding.ensureInitialized();
+  });
+
+  testWidgets('customize highlight color', (tester) async {
+    const text = 'Hello World';
+    final document = Document.blank()
+      ..insert(
+        [0],
+        [bulletedListNode(text: text)],
+      );
+    final editorState = EditorState(document: document);
+    final widget = CustomToolbarItemColor(
+      editorState: editorState,
+    );
+    await tester.pumpWidget(widget);
+    await tester.pumpAndSettle();
+
+    // update selection and show the toolbar
+    editorState.updateSelectionWithReason(
+      Selection.single(path: [0], startOffset: 0, endOffset: text.length),
+    );
+    await tester.pumpAndSettle(const Duration(milliseconds: 500));
+    expect(find.byType(FloatingToolbar), findsOneWidget);
+    final bulletedListItem = tester.widget<SVGIconItemWidget>(
+      find.byType(SVGIconItemWidget),
+    );
+    expect(bulletedListItem.highlightColor, Colors.green);
+  });
+}
+
+class CustomToolbarItemColor extends StatelessWidget {
+  CustomToolbarItemColor({
+    super.key,
+    required this.editorState,
+  });
+
+  final EditorState editorState;
+  final scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: const [
+        AppFlowyEditorLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en', 'US')],
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            width: 500,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.blue),
+            ),
+            child: FloatingToolbar(
+              items: [bulletedListItem],
+              style: const FloatingToolbarStyle(
+                backgroundColor: Colors.red,
+                toolbarActiveColor: Colors.green,
+              ),
+              editorState: editorState,
+              scrollController: scrollController,
+              child: AppFlowyEditor(
+                editorState: editorState,
+                scrollController: scrollController,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
