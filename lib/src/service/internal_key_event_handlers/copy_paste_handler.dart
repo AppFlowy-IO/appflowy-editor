3c64f299d63c2fc342b9194a41a0ba375d181542
Lucas.Xu <lucas.xu@appflowy.io>
2023-07-20 17:05:59 +0800

    fix: unable to paste multiple lines

--- lib/src/service/internal_key_event_handlers/copy_paste_handler.dart
+++ lib/src/service/internal_key_event_handlers/copy_paste_handler.dart
@@ -18,7 +18,40 @@ void _pasteSingleLine(
   editorState.apply(transaction);
 }
 
-void _pasteMarkdown(EditorState editorState, String markdown) {}
+void _pasteMarkdown(EditorState editorState, String markdown) {
+  final selection = editorState.selection;
+  if (selection == null) {
+    return;
+  }
+
+  final lines = markdown.split('\n');
+
+  if (lines.length == 1) {
+    _pasteSingleLine(editorState, selection, lines[0]);
+    return;
+  }
+
+  var path = selection.end.path.next;
+  final node = editorState.document.nodeAtPath(selection.end.path);
+  final delta = node?.delta;
+  if (delta != null && delta.toPlainText().isEmpty) {
+    path = selection.end.path;
+  }
+  final document = markdownToDocument(markdown);
+  final transaction = editorState.transaction;
+  var afterPath = path;
+  for (var i = 0; i < document.root.children.length - 1; i++) {
+    afterPath = afterPath.next;
+  }
+  final offset = document.root.children.lastOrNull?.delta?.length ?? 0;
+  transaction
+    ..insertNodes(path, document.root.children)
+    ..afterSelection = Selection.collapse(
+      afterPath,
+      offset,
+    );
+  editorState.apply(transaction);
+}
 
 void handlePastePlainText(EditorState editorState, String plainText) {
   final selection = editorState.selection?.normalized;
