cce8745c107d77d3f6b83c5364b4ad509b56d82e
Lucas.Xu <lucas.xu@appflowy.io>
2023-07-25 15:15:34 +0800

    feat: support scroll without animation

--- lib/src/editor/editor_component/service/scroll_service_widget.dart
+++ lib/src/editor/editor_component/service/scroll_service_widget.dart
@@ -121,7 +121,23 @@ class _ScrollServiceWidgetState extends State<ScrollServiceWidget>
             startAutoScroll(endTouchPoint, edgeOffset: 50);
           });
         } else {
-          startAutoScroll(endTouchPoint, edgeOffset: 100);
+          if (editorState.selectionType == SelectionType.block) {
+            final box = editorState.renderBox;
+            final editorOffset = box?.localToGlobal(Offset.zero);
+            final editorHeight = box?.size.height;
+            double offset = 100;
+            if (editorOffset != null && editorHeight != null) {
+              // try to center the highlight area
+              offset = editorOffset.dy + editorHeight / 2.0;
+            }
+            startAutoScroll(
+              endTouchPoint,
+              edgeOffset: offset,
+              duration: Duration.zero,
+            );
+          } else {
+            startAutoScroll(endTouchPoint, edgeOffset: 100);
+          }
         }
       } else {
         startAutoScroll(endTouchPoint);
@@ -159,11 +175,13 @@ class _ScrollServiceWidgetState extends State<ScrollServiceWidget>
     Offset offset, {
     double edgeOffset = 100,
     AxisDirection? direction,
+    Duration? duration,
   }) =>
       forward.startAutoScroll(
         offset,
         edgeOffset: edgeOffset,
         direction: direction,
+        duration: duration,
       );
 
   @override
