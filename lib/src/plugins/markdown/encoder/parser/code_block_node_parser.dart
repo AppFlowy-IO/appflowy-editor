b26a2ea99fc0c2453f365c0ea8e7bd4a4292506b
Mukund Tandon <71614009+Mukund-Tandon@users.noreply.github.com>
2023-08-01 07:26:16 +0530

    fix: fixed the issue of code block not being exported in markdown (#347)

--- lib/src/plugins/markdown/encoder/parser/code_block_node_parser.dart
+++ lib/src/plugins/markdown/encoder/parser/code_block_node_parser.dart
@@ -4,18 +4,19 @@ class CodeBlockNodeParser extends NodeParser {
   const CodeBlockNodeParser();
 
   @override
-  String get id => 'code_block';
+  String get id => 'code';
 
   @override
   String transform(Node node) {
-    assert(node.type == 'code_block');
+    assert(node.type == 'code');
 
     final delta = node.delta;
+    final language = node.attributes['language'] ?? '';
     if (delta == null) {
       throw Exception('Delta is null');
     }
     final markdown = DeltaMarkdownEncoder().convert(delta);
-    final result = '```\n$markdown\n```';
+    final result = '```$language\n$markdown\n```';
     final suffix = node.next == null ? '' : '\n';
 
     return '$result$suffix';
