7e39d1bcf9c36c39e727381050df5e0a5378a908
Mayur Mahajan <47064215+MayurSMahajan@users.noreply.github.com>
2023-08-02 07:33:03 +0530

    feat: paste plaintext shortcut (#338)
    * feat: shortcut for pasting plaintext
    
    * test: paste without formatting
    
    * chore: use text directly
    
    * refactor: add cut command

--- lib/src/editor/editor_component/service/shortcuts/command_shortcut_events/paste_command.dart
+++ lib/src/editor/editor_component/service/shortcuts/command_shortcut_events/paste_command.dart
@@ -1,6 +1,11 @@
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:flutter/material.dart';
 
+final List<CommandShortcutEvent> pasteCommands = [
+  pasteCommand,
+  pasteTextWithoutFormattingCommand,
+];
+
 /// Paste.
 ///
 /// - support
@@ -13,6 +18,52 @@ final CommandShortcutEvent pasteCommand = CommandShortcutEvent(
   macOSCommand: 'cmd+v',
   handler: _pasteCommandHandler,
 );
+
+final CommandShortcutEvent pasteTextWithoutFormattingCommand =
+    CommandShortcutEvent(
+  key: 'paste the content',
+  command: 'ctrl+shift+v',
+  macOSCommand: 'cmd+shift+v',
+  handler: _pasteTextWithoutFormattingCommandHandler,
+);
+
+CommandShortcutEventHandler _pasteTextWithoutFormattingCommandHandler =
+    (editorState) {
+  if (PlatformExtension.isMobile) {
+    assert(
+      false,
+      'pasteTextWithoutFormattingCommand is not supported on mobile platform.',
+    );
+    return KeyEventResult.ignored;
+  }
+
+  var selection = editorState.selection;
+  if (selection == null) {
+    return KeyEventResult.ignored;
+  }
+
+  // delete the selection first.
+  if (!selection.isCollapsed) {
+    editorState.deleteSelection(selection);
+  }
+
+  // fetch selection again.
+  selection = editorState.selection;
+  if (selection == null) {
+    return KeyEventResult.skipRemainingHandlers;
+  }
+  assert(selection.isCollapsed);
+
+  () async {
+    final data = await AppFlowyClipboard.getData();
+    final text = data.text;
+    if (text != null && text.isNotEmpty) {
+      handlePastePlainText(editorState, text);
+    }
+  }();
+
+  return KeyEventResult.handled;
+};
 
 CommandShortcutEventHandler _pasteCommandHandler = (editorState) {
   if (PlatformExtension.isMobile) {
