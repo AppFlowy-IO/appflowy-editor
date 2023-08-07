7bbe3280b73eebb3bbb57c5d4ac5471ab4865de8
Mathias Mogensen <mathiasrieckm@gmail.com>
2023-08-07 13:17:27 +0200

    test: coverage for position.dart

--- test/core/location/position_test.dart
+++ test/core/location/position_test.dart
@@ -5,6 +5,28 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() async {
   group('position.dart', () {
+    test('Position.invalid', () {
+      final invalid = Position.invalid();
+
+      expect(invalid.path, [-1]);
+      expect(invalid.offset, -1);
+    });
+
+    test('hashCode', () {
+      final p1 = Position(path: [0], offset: 5);
+      final p2 = Position(path: [0], offset: 5);
+
+      expect(p1 == p2, true);
+      expect(p1.hashCode == p2.hashCode, true);
+    });
+
+    test('toString', () {
+      final p1 = Position(path: [0], offset: 5);
+      const str = 'path = [0], offset = 5';
+
+      expect(p1.toString(), str);
+    });
+
     test('test position equality', () {
       final positionA = Position(path: [0, 1, 2], offset: 3);
       final positionB = Position(path: [0, 1, 2], offset: 3);
