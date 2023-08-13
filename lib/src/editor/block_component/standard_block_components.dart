c11bac806d3172d1055ef48cc2d1b37e3b1cc9fa
MayurSMahajan <mayur23smahajan@gmail.com>
2023-08-13 10:20:53 +0530

    feat: open link command

--- lib/src/editor/block_component/standard_block_components.dart
+++ lib/src/editor/block_component/standard_block_components.dart
@@ -112,6 +112,7 @@ final List<CommandShortcutEvent> standardCommandShortcutEvents = [
   toggleTodoListCommand,
   ...toggleMarkdownCommands,
   showLinkMenuCommand,
+  openInlineLinkCommand,
 
   //
   indentCommand,
