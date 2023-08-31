a7f49b8f34ab08299cf51abbcdbb8587f8b95a86
Mohammad Zolfaghari <zidom72@gmail.com>
2023-08-31 18:58:58 +0330

    fix: modify scroll view padding

--- lib/src/editor/block_component/table_block_component/table_block_component.dart
+++ lib/src/editor/block_component/table_block_component/table_block_component.dart
@@ -144,7 +144,7 @@ class _TableBlockComponentWidgetState extends State<TableBlockComponentWidget>
     Widget child = Scrollbar(
       controller: _scrollController,
       child: SingleChildScrollView(
-        padding: const EdgeInsets.all(6),
+        padding: const EdgeInsets.only(top: 10, left: 10, bottom: 4),
         controller: _scrollController,
         scrollDirection: Axis.horizontal,
         child: TableView(
