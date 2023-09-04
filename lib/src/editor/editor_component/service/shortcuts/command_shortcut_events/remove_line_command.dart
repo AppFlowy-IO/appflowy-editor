a33f49a0e4fbf826a984aee957ed5d1b85438249
MayurSMahajan <mayur23smahajan@gmail.com>
2023-09-04 10:32:03 +0530

    refactor: separate remove line command

---
+++ lib/src/editor/editor_component/service/shortcuts/command_shortcut_events/remove_line_command.dart
@@ -0,0 +1,39 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:flutter/material.dart';
+
+/// Remove Line to the Left.
+///
+/// - support
+///   - desktop
+///   - web
+///   - mobile
+///
+
+final CommandShortcutEvent deleteLeftSentenceCommand = CommandShortcutEvent(
+  key: 'delete the left line',
+  command: 'ctrl+alt+backspace',
+  macOSCommand: 'cmd+backspace',
+  handler: _deleteLeftSentenceCommandHandler,
+);
+
+CommandShortcutEventHandler _deleteLeftSentenceCommandHandler = (editorState) {
+  final selection = editorState.selection;
+  if (selection == null || !selection.isCollapsed) {
+    return KeyEventResult.ignored;
+  }
+
+  final node = editorState.getNodeAtPath(selection.end.path);
+  final delta = node?.delta;
+  if (node == null || delta == null) {
+    return KeyEventResult.ignored;
+  }
+
+  final transaction = editorState.transaction;
+  transaction.deleteText(
+    node,
+    0,
+    selection.endIndex,
+  );
+  editorState.apply(transaction);
+  return KeyEventResult.handled;
+};
