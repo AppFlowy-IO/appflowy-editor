56df5822cc0b47a81f162e61a723768227130617
Lucas.Xu <lucas.xu@appflowy.io>
2023-09-12 11:28:50 +0800

    feat: refactor color conversion method to handle RGB and hex formatsRefactor method to handle RGB and hex formats, improving color conversion

--- test/editor/util/color_util_test.dart
+++ test/editor/util/color_util_test.dart
@@ -4,12 +4,52 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('ColorExtension', () {
-    test('tryToColor', () {
-      const validRgba = "rgba(255, 255, 255, 1)";
-      const invalidRgba = "rgba(255, 255, 0)";
+    test('Test RGBA color conversion', () {
+      final rgba1 = 'rgba(0, 128, 255, 0.5)'.tryToColor();
+      final rgba2 = 'rgba(255, 255, 0)'.tryToColor();
 
-      expect(validRgba.tryToColor(), const Color.fromARGB(255, 255, 255, 255));
-      expect(invalidRgba.tryToColor(), null);
+      expect(
+        rgba1,
+        equals(const Color(0x7f0080ff)),
+      );
+      expect(
+        rgba2,
+        equals(null),
+      );
+    });
+
+    test('Test RGB color conversion', () {
+      final rgb1 = 'rgb(255, 0, 0)'.tryToColor();
+      final rgb2 = 'rgb(255, 0, 0, 255)'.tryToColor();
+      expect(
+        rgb1,
+        equals(const Color(0xffff0000)),
+      );
+      expect(
+        rgb2,
+        equals(null),
+      );
+    });
+
+    test('Test hex color conversion (0x format)', () {
+      final hex1 = '0xFF00FF'.tryToColor();
+      final hex2 = '0x0000FFFF'.tryToColor();
+      expect(
+        hex1,
+        equals(const Color(0xffff00ff)),
+      );
+      expect(
+        hex2,
+        equals(const Color(0x0000FFFF)),
+      );
+    });
+
+    test('Test hex color conversion (# format)', () {
+      final hex1 = '#00FF00'.tryToColor();
+      expect(
+        hex1,
+        equals(const Color(0xff00ff00)),
+      );
     });
   });
 
