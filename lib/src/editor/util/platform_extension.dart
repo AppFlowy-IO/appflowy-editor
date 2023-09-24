fe5043e9610399b9ab8886ade0a79c3d46e38711
Lucas.Xu <lucas.xu@appflowy.io>
2023-09-24 10:51:42 +0800

    fix: command shortcuts on the web platform should also respect the operating system

--- lib/src/editor/util/platform_extension.dart
+++ lib/src/editor/util/platform_extension.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:universal_html/html.dart' show window;
 
 // TODO(Xazin): Refactor to honor `Theme.platform`
 extension PlatformExtension on Platform {
+  static String get _webPlatform =>
+      window.navigator.platform?.toLowerCase() ?? '';
+
+  /// Returns true if the operating system is macOS and not running on Web platform.
   static bool get isMacOS {
     if (kIsWeb) {
       return false;
@@ -11,11 +16,44 @@ extension PlatformExtension on Platform {
     return Platform.isMacOS;
   }
 
+  /// Returns true if the operating system is Windows and not running on Web platform.
   static bool get isWindows {
     if (kIsWeb) {
       return false;
     }
     return Platform.isWindows;
+  }
+
+  /// Returns true if the operating system is Linux and not running on Web platform.
+  static bool get isLinux {
+    if (kIsWeb) {
+      return false;
+    }
+    return Platform.isWindows;
+  }
+
+  /// Returns true if the operating system is macOS and running on Web platform.
+  static bool get isWebOnMacOS {
+    if (!kIsWeb) {
+      return false;
+    }
+    return _webPlatform.contains('mac') == true;
+  }
+
+  /// Returns true if the operating system is Windows and running on Web platform.
+  static bool get isWebOnWindows {
+    if (!kIsWeb) {
+      return false;
+    }
+    return _webPlatform.contains('windows') == true;
+  }
+
+  /// Returns true if the operating system is Linux and running on Web platform.
+  static bool get isWebOnLinux {
+    if (!kIsWeb) {
+      return false;
+    }
+    return _webPlatform.contains('linux') == true;
   }
 
   static bool get isDesktopOrWeb {
