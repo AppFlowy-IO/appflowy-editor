22559d36b68b5bfbf52a4e081eae3e83010588e8
Lucas.Xu <lucas.xu@appflowy.io>
2023-10-10 16:36:03 +0800

    fix: the selection should be clear if header or footer is focusing

---
+++ lib/src/editor/block_component/base_component/widget/ignore_parent_gesture.dart
@@ -0,0 +1,54 @@
+import 'dart:math';
+
+import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class IgnoreEditorSelectionGesture extends StatefulWidget {
+  const IgnoreEditorSelectionGesture({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  State<IgnoreEditorSelectionGesture> createState() =>
+      _IgnoreEditorSelectionGestureState();
+}
+
+class _IgnoreEditorSelectionGestureState
+    extends State<IgnoreEditorSelectionGesture> {
+  final key = Random(10000).toString();
+  late final SelectionGestureInterceptor interceptor;
+  late final EditorState editorState = context.read<EditorState>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    interceptor = SelectionGestureInterceptor(
+      key: key,
+      canTap: (details) {
+        final renderObject = context.findRenderObject();
+        if (renderObject != null && renderObject is RenderBox) {
+          final offset = renderObject.globalToLocal(details.globalPosition);
+          return !renderObject.paintBounds.contains(offset);
+        }
+        return true;
+      },
+    );
+    editorState.selectionService.registerGestureInterceptor(interceptor);
+  }
+
+  @override
+  void dispose() {
+    editorState.selectionService.unregisterGestureInterceptor(key);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
