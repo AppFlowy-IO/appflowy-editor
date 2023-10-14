9ae85ea162606b79483c49550266c154c0cb500c
Lucas.Xu <lucas.xu@appflowy.io>
2023-10-14 23:47:57 +0800

    feat: support parsing the nested list when exporting the document to markdown format (#540)
    * feat: support exporting nested list
    
    * feat: support parsing the nested list
    
    * fix: tests

--- test/plugins/markdown/encoder/parser/table_node_parser_test.dart
+++ test/plugins/markdown/encoder/parser/table_node_parser_test.dart
@@ -1,5 +1,5 @@
-import 'package:appflowy_editor/src/plugins/markdown/encoder/parser/table_node_parser.dart';
 import 'package:appflowy_editor/src/editor/block_component/table_block_component/table_node.dart';
+import 'package:appflowy_editor/src/plugins/markdown/encoder/parser/table_node_parser.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() async {
@@ -10,7 +10,7 @@ void main() async {
         ['b'],
       ]).node;
 
-      final result = const TableNodeParser().transform(node);
+      final result = const TableNodeParser().transform(node, null);
       expect(result, '|a|b|\n|-|-|');
     });
 
@@ -20,7 +20,7 @@ void main() async {
         [''],
       ]).node;
 
-      final result = const TableNodeParser().transform(node);
+      final result = const TableNodeParser().transform(node, null);
       expect(result, '| | |\n|-|-|');
     });
   });
