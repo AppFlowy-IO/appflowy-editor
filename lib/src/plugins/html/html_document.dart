522f17a00d7d2d529192745bf97a107b9e85d699
Ali Hassan <71745182+alihassan143@users.noreply.github.com>
2023-10-22 18:59:26 +0500

    feat: table html encoder and decoder added (#449)
    * feat: table html encoder and decoder added
    
    * feat: html table encoder test added
    
    * feat: fix the test ran issues
    
    * fix: removed unused code

--- lib/src/plugins/html/html_document.dart
+++ lib/src/plugins/html/html_document.dart
@@ -28,6 +28,7 @@ String documentToHTML(
       const HTMLQuoteNodeParser(),
       const HTMLHeadingNodeParser(),
       const HTMLImageNodeParser(),
+      const HtmlTableNodeParser()
     ],
   ).encode(document);
 }
