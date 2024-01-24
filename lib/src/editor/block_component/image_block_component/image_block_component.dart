cde0f60a730097eea7f64de9c98a5f512a3bec68
Jayaprakash <jayaprakash07.dev@gmail.com>
2024-01-25 01:43:52 +0530

    refactor: formatted code file using dart formatter

--- lib/src/editor/block_component/image_block_component/image_block_component.dart
+++ lib/src/editor/block_component/image_block_component/image_block_component.dart
@@ -197,7 +197,8 @@ class ImageBlockComponentWidgetState extends State<ImageBlockComponentWidget>
                   node: node,
                   delegate: this,
                   listenable: editorState.selectionNotifier,
-                  dragAndDropListenable: editorState.dragAndDropSelectionNotifier,
+                  dragAndDropListenable:
+                      editorState.dragAndDropSelectionNotifier,
                   cursorColor: editorState.editorStyle.cursorColor,
                   selectionColor: editorState.editorStyle.selectionColor,
                   child: child!,
