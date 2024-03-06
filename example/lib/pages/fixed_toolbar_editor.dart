4e13a906fc23133c3b398dd5bfb60308aecf56a9
Lucas.Xu <lucas.xu@appflowy.io>
2024-03-07 00:12:51 +0800

    feat: highlight bold button if the selected text is all bold

--- example/lib/pages/fixed_toolbar_editor.dart
+++ example/lib/pages/fixed_toolbar_editor.dart
@@ -102,97 +102,137 @@ class _FixedToolbar extends StatelessWidget {
       Icons.code,
       Icons.horizontal_rule,
     ];
-    return ListView.separated(
-      scrollDirection: Axis.horizontal,
-      itemBuilder: (_, index) => IconButton(
-        icon: Icon(items[index]),
-        onPressed: () {
-          debugPrint(items[index].toString());
 
-          switch (items[index]) {
-            case Icons.format_bold:
-              editorState.toggleAttribute(AppFlowyRichTextKeys.bold);
-              break;
-            case Icons.format_italic:
-              editorState.toggleAttribute(AppFlowyRichTextKeys.italic);
-              break;
-            case Icons.format_underlined:
-              editorState.toggleAttribute(AppFlowyRichTextKeys.underline);
-              break;
-            case Icons.format_strikethrough:
-              editorState.toggleAttribute(AppFlowyRichTextKeys.strikethrough);
-              break;
-            case Icons.text_fields:
-              editorState.formatNode(null, (node) {
-                return node.copyWith(
-                  type: ParagraphBlockKeys.type,
-                );
-              });
-              break;
-            case Icons.format_list_bulleted:
-              editorState.formatNode(null, (node) {
-                return node.copyWith(
-                  type: node.type == BulletedListBlockKeys.type
-                      ? ParagraphBlockKeys.type
-                      : BulletedListBlockKeys.type,
-                );
-              });
-              break;
-            case Icons.format_list_numbered:
-              editorState.formatNode(null, (node) {
-                return node.copyWith(
-                  type: node.type == NumberedListBlockKeys.type
-                      ? ParagraphBlockKeys.type
-                      : NumberedListBlockKeys.type,
-                );
-              });
-              break;
-            case Icons.format_align_left:
-              editorState.formatNode(null, (node) {
-                return node.copyWith(
-                  attributes: {
-                    ...node.attributes,
-                    blockComponentAlign: 'left',
-                  },
-                );
-              });
-              break;
-            case Icons.format_align_center:
-              editorState.formatNode(null, (node) {
-                return node.copyWith(
-                  attributes: {
-                    ...node.attributes,
-                    blockComponentAlign: 'center',
-                  },
-                );
-              });
-              break;
-            case Icons.format_align_right:
-              editorState.formatNode(null, (node) {
-                return node.copyWith(
-                  attributes: {
-                    ...node.attributes,
-                    blockComponentAlign: 'right',
-                  },
-                );
-              });
-              break;
-            case Icons.horizontal_rule:
-              final selection = editorState.selection;
-              if (selection == null) {
-                return;
-              }
-              final transaction = editorState.transaction;
-              transaction.insertNode(selection.start.path.next, dividerNode());
-              editorState.apply(transaction);
-              break;
-            default:
-              break;
-          }
-        },
-      ),
-      separatorBuilder: (_, __) => const VerticalDivider(),
-      itemCount: items.length,
+    return ValueListenableBuilder(
+      valueListenable: editorState.selectionNotifier,
+      builder: (context, selection, _) {
+        return ListView.separated(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (_, index) {
+            final isBold = _isTextDecorationActive(
+              editorState,
+              selection,
+              AppFlowyRichTextKeys.bold,
+            );
+            return IconButton(
+              icon: Icon(items[index]),
+              color: items[index] == Icons.format_bold && isBold
+                  ? Colors.blue
+                  : Colors.black,
+              onPressed: () {
+                debugPrint(items[index].toString());
+                switch (items[index]) {
+                  case Icons.format_bold:
+                    editorState.toggleAttribute(AppFlowyRichTextKeys.bold);
+                    break;
+                  case Icons.format_italic:
+                    editorState.toggleAttribute(AppFlowyRichTextKeys.italic);
+                    break;
+                  case Icons.format_underlined:
+                    editorState.toggleAttribute(AppFlowyRichTextKeys.underline);
+                    break;
+                  case Icons.format_strikethrough:
+                    editorState
+                        .toggleAttribute(AppFlowyRichTextKeys.strikethrough);
+                    break;
+                  case Icons.text_fields:
+                    editorState.formatNode(null, (node) {
+                      return node.copyWith(
+                        type: ParagraphBlockKeys.type,
+                      );
+                    });
+                    break;
+                  case Icons.format_list_bulleted:
+                    editorState.formatNode(null, (node) {
+                      return node.copyWith(
+                        type: node.type == BulletedListBlockKeys.type
+                            ? ParagraphBlockKeys.type
+                            : BulletedListBlockKeys.type,
+                      );
+                    });
+                    break;
+                  case Icons.format_list_numbered:
+                    editorState.formatNode(null, (node) {
+                      return node.copyWith(
+                        type: node.type == NumberedListBlockKeys.type
+                            ? ParagraphBlockKeys.type
+                            : NumberedListBlockKeys.type,
+                      );
+                    });
+                    break;
+                  case Icons.format_align_left:
+                    editorState.formatNode(null, (node) {
+                      return node.copyWith(
+                        attributes: {
+                          ...node.attributes,
+                          blockComponentAlign: 'left',
+                        },
+                      );
+                    });
+                    break;
+                  case Icons.format_align_center:
+                    editorState.formatNode(null, (node) {
+                      return node.copyWith(
+                        attributes: {
+                          ...node.attributes,
+                          blockComponentAlign: 'center',
+                        },
+                      );
+                    });
+                    break;
+                  case Icons.format_align_right:
+                    editorState.formatNode(null, (node) {
+                      return node.copyWith(
+                        attributes: {
+                          ...node.attributes,
+                          blockComponentAlign: 'right',
+                        },
+                      );
+                    });
+                    break;
+                  case Icons.horizontal_rule:
+                    final selection = editorState.selection;
+                    if (selection == null) {
+                      return;
+                    }
+                    final transaction = editorState.transaction;
+                    transaction.insertNode(
+                      selection.start.path.next,
+                      dividerNode(),
+                    );
+                    editorState.apply(transaction);
+                    break;
+                  default:
+                    break;
+                }
+              },
+            );
+          },
+          separatorBuilder: (_, __) => const VerticalDivider(),
+          itemCount: items.length,
+        );
+      },
     );
+  }
+
+  bool _isTextDecorationActive(
+    EditorState editorState,
+    Selection? selection,
+    String name,
+  ) {
+    selection = selection ?? editorState.selection;
+    if (selection == null) {
+      return false;
+    }
+    final nodes = editorState.getNodesInSelection(selection);
+    if (selection.isCollapsed) {
+      return editorState.toggledStyle.containsKey(name);
+    } else {
+      return nodes.allSatisfyInSelection(selection, (delta) {
+        return delta.everyAttributes(
+          (attributes) => attributes[name] == true,
+        );
+      });
+    }
   }
 }
