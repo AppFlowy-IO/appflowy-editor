5bf6c7acd066774f204fe6c8bd4c574e391f646a
Lucas.Xu <lucas.xu@appflowy.io>
2024-07-04 09:53:25 +0800

    feat: support custom markdown parsers (#835)
    * feat: support custom markdown parsers
    
    * fix: markdown test
    
    * feat: add markdown quote parser
    
    * chore: use markdown parser
    
    * chore: parse unordered list, to_do list and quote
    
    * feat: add a markdown editor example
    
    * feat: add bulleted list parser
    
    * feat: support nested list and img
    
    * feat: support code block
    
    * feat: support table parser
    
    * chore: refacotr code
    
    * fix: flutter analyze
    
    * chore: add more tests and remove unused files
    
    * fix: test

---
+++ lib/src/plugins/markdown/decoder/parser/custom_markdown_node_parser.dart
@@ -0,0 +1,12 @@
+import 'package:appflowy_editor/appflowy_editor.dart';
+import 'package:markdown/markdown.dart' as md;
+
+abstract class CustomMarkdownParser {
+  const CustomMarkdownParser();
+
+  List<Node> transform(
+    md.Node element,
+    List<CustomMarkdownParser> parsers,
+    MarkdownListType listType,
+  );
+}
