8e17d1447eea0b57ff92e31dbe88796ce759fb37
Lucas.Xu <lucas.xu@appflowy.io>
2024-08-16 10:52:48 +0800

    feat: optimize table update (#869)
    * feat: optimize table update
    
    * fix: combine the table transactions

--- lib/src/editor/block_component/table_block_component/table_col.dart
+++ lib/src/editor/block_component/table_block_component/table_col.dart
@@ -1,7 +1,6 @@
 import 'package:appflowy_editor/appflowy_editor.dart';
 import 'package:appflowy_editor/src/editor/block_component/table_block_component/table_action_handler.dart';
 import 'package:appflowy_editor/src/editor/block_component/table_block_component/table_col_border.dart';
-import 'package:appflowy_editor/src/editor/block_component/table_block_component/table_node.dart';
 import 'package:appflowy_editor/src/editor/block_component/table_block_component/util.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
