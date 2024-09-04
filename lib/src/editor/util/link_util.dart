ff8aa3e72fba4cce7899bc397d2cb466e195eb8f
SillyCoon <self-councious@proton.me>
2024-09-04 19:39:54 +0900

    feat: add file link input support

--- lib/src/editor/util/link_util.dart
+++ lib/src/editor/util/link_util.dart
@@ -1,11 +1,15 @@
 import 'package:string_validator/string_validator.dart';
 
 bool isUri(String text) {
-  return isURL(text) || isMailTo(text);
+  return isURL(text) || isMailTo(text) || isFile(text);
 }
 
 bool isMailTo(String text) {
   const mailToPrefix = 'mailto:';
   return text.startsWith(mailToPrefix) &&
       isEmail(text.substring(mailToPrefix.length));
+}
+
+bool isFile(String text) {
+  return text.startsWith('file:/');
 }
