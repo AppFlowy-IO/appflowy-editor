f943c48a9da93809fb1281bd8f3af64e67afa038
Lucas.Xu <lucas.xu@appflowy.io>
2024-09-09 09:46:39 +0800

    fix: unit test and code format

--- lib/src/editor/util/link_util.dart
+++ lib/src/editor/util/link_util.dart
@@ -2,7 +2,7 @@ import 'package:string_validator/string_validator.dart';
 
 bool isUri(String text) {
   final lowerText = text.toLowerCase();
-  return isURL(text) || 
-         lowerText.startsWith('mailto:') || 
-         lowerText.startsWith('file:');
+  return isURL(text) ||
+      lowerText.startsWith('mailto:') ||
+      lowerText.startsWith('file:');
 }
