8451439aade0ef4c93e039db05e7d65a777a87fc
Lucas.Xu <lucas.xu@appflowy.io>
2024-09-09 09:19:16 +0800

    chore: update lib/src/editor/util/link_util.dart

--- lib/src/editor/util/link_util.dart
+++ lib/src/editor/util/link_util.dart
@@ -1,15 +1,8 @@
 import 'package:string_validator/string_validator.dart';
 
 bool isUri(String text) {
-  return isURL(text) || isMailTo(text) || isFile(text);
-}
-
-bool isMailTo(String text) {
-  const mailToPrefix = 'mailto:';
-  return text.startsWith(mailToPrefix) &&
-      isEmail(text.substring(mailToPrefix.length));
-}
-
-bool isFile(String text) {
-  return text.startsWith('file:/');
+  final lowerText = text.toLowerCase();
+  return isURL(text) || 
+         lowerText.startsWith('mailto:') || 
+         lowerText.startsWith('file:');
 }
