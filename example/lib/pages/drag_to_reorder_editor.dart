943299217854e4a8dd4f23ddaa2b55464a6e3bde
Lucas.Xu <lucas.xu@appflowy.io>
2024-09-20 14:55:52 +0800

    feat: support moving a block to become a child of another block

--- example/lib/pages/drag_to_reorder_editor.dart
+++ example/lib/pages/drag_to_reorder_editor.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
+enum HorizontalPosition { left, center, right }
+
+enum VerticalPosition { top, middle, bottom }
+
 class DragToReorderEditor extends StatefulWidget {
   const DragToReorderEditor({
     super.key,
@@ -156,6 +160,7 @@ class _DragToReorderActionState extends State<DragToReorderAction> {
         onDragUpdate: (details) {
           editorState.selectionService.renderDropTargetForOffset(
             details.globalPosition,
+            builder: _buildDropArea,
           );
 
           globalPosition = details.globalPosition;
@@ -192,6 +197,16 @@ class _DragToReorderActionState extends State<DragToReorderAction> {
   Future<void> _moveNodeToNewPosition(Node node, Path? acceptedPath) async {
     if (acceptedPath == null) {
       debugPrint('acceptedPath is null');
+      return;
+    }
+
+    if (node.path.equals(acceptedPath)) {
+      debugPrint('node($node) is already at path($acceptedPath)');
+      return;
+    }
+
+    if (node.path.isAncestorOf(acceptedPath)) {
+      debugPrint('node($node) is ancestor of path($acceptedPath)');
       return;
     }
 
@@ -234,6 +249,89 @@ class _DragToReorderActionState extends State<DragToReorderAction> {
         color: Colors.transparent,
         child: child,
       ),
+    );
+  }
+
+  Widget _buildDropArea(BuildContext context, DragAreaBuilderData data) {
+    final node = data.targetNode;
+    final selectable = node.selectable;
+
+    if (selectable == null) {
+      return const SizedBox.shrink();
+    }
+
+    final renderBox = selectable.context.findRenderObject() as RenderBox?;
+    if (renderBox == null) {
+      return const SizedBox.shrink();
+    }
+
+    final globalBlockOffset = renderBox.localToGlobal(Offset.zero);
+    final globalBlockRect = globalBlockOffset & renderBox.size;
+    final dragOffset = data.dragOffset;
+
+    // Check if the dragOffset is within the globalBlockRect
+    final isInside = globalBlockRect.contains(dragOffset);
+
+    if (!isInside) {
+      return const SizedBox.shrink();
+    }
+
+    // Determine the relative position
+    HorizontalPosition horizontalPosition;
+    VerticalPosition verticalPosition;
+
+    // Horizontal position
+    if (dragOffset.dx < globalBlockRect.left + 44) {
+      horizontalPosition = HorizontalPosition.left;
+    } else {
+      // ignore the middle here, it's not used in this example
+      horizontalPosition = HorizontalPosition.right;
+    }
+
+    // Vertical position
+    if (dragOffset.dy < globalBlockRect.top + globalBlockRect.height / 2) {
+      verticalPosition = VerticalPosition.top;
+    } else {
+      verticalPosition = VerticalPosition.bottom;
+    }
+
+    // 44 is the width of the drag indicator
+    const indicatorWidth = 44.0;
+    final width = globalBlockRect.width - indicatorWidth;
+
+    Widget child = Container(
+      height: 2,
+      width: width,
+      color: Colors.red,
+    );
+
+    if (horizontalPosition == HorizontalPosition.right) {
+      const breakWidth = 22.0;
+      const padding = 8.0;
+      child = Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            height: 2,
+            width: breakWidth,
+            color: Colors.red,
+          ),
+          const SizedBox(width: padding),
+          Container(
+            height: 2,
+            width: width - breakWidth - padding,
+            color: Colors.red,
+          ),
+        ],
+      );
+    }
+
+    return Positioned(
+      top: verticalPosition == VerticalPosition.top
+          ? globalBlockRect.top
+          : globalBlockRect.bottom,
+      left: globalBlockRect.left + 22,
+      child: child,
     );
   }
 }
