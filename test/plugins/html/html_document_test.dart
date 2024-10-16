35f0f6637036431cb8a24daa16d687cad58698a8
Lucas <lucas.xu@appflowy.io>
2024-10-16 10:22:40 +0800

    fix: html decoder ignores h4-h6 tags (#932)

--- test/plugins/html/html_document_test.dart
+++ test/plugins/html/html_document_test.dart
@@ -304,4 +304,22 @@ void main() {
       ],
     );
   });
+
+  test('sample 11', () {
+    const html = '''
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<h5>Heading 5</h5>
+<h6>Heading 6</h6>
+''';
+    final document = htmlToDocument(html);
+    for (var i = 1; i <= 6; i++) {
+      final node = document.nodeAtPath([i - 1]);
+      expect(node!.type, HeadingBlockKeys.type);
+      expect(node.delta!.toPlainText(), 'Heading $i');
+      expect(node.attributes[HeadingBlockKeys.level], i);
+    }
+  });
 }
