9e0e71e77437ec054cf897f7197ed450aba372a5
Abdul Ahad <ahadpatel92@gmail.com>
2024-10-16 19:28:34 +0530

    chore: add appropriate description

--- lib/src/editor/block_component/heading_block_component/heading_command_shortcut.dart
+++ lib/src/editor/block_component/heading_block_component/heading_command_shortcut.dart
@@ -20,7 +20,7 @@ final List<CommandShortcutEvent> toggleHeadingCommands = [
 ///
 final CommandShortcutEvent toggleH1 = CommandShortcutEvent(
   key: 'toggle H1',
-  getDescription: () => AppFlowyEditorL10n.current.cmdToggleCode,
+  getDescription: () => AppFlowyEditorL10n.current.cmdToggleH1,
   command: 'ctrl+shift+t',
   macOSCommand: 'cmd+shift+t',
   handler: (editorState) => _toggleAttribute(
@@ -31,7 +31,7 @@ final CommandShortcutEvent toggleH1 = CommandShortcutEvent(
 
 final CommandShortcutEvent toggleH2 = CommandShortcutEvent(
   key: 'toggle H2',
-  getDescription: () => AppFlowyEditorL10n.current.cmdToggleCode,
+  getDescription: () => AppFlowyEditorL10n.current.cmdToggleH2,
   command: 'ctrl+shift+g',
   macOSCommand: 'cmd+shift+g',
   handler: (editorState) => _toggleAttribute(
@@ -42,7 +42,7 @@ final CommandShortcutEvent toggleH2 = CommandShortcutEvent(
 
 final CommandShortcutEvent toggleH3 = CommandShortcutEvent(
   key: 'toggle H3',
-  getDescription: () => AppFlowyEditorL10n.current.cmdToggleCode,
+  getDescription: () => AppFlowyEditorL10n.current.cmdToggleH3,
   command: 'ctrl+shift+j',
   macOSCommand: 'cmd+shift+j',
   handler: (editorState) => _toggleAttribute(
@@ -53,7 +53,7 @@ final CommandShortcutEvent toggleH3 = CommandShortcutEvent(
 
 final CommandShortcutEvent toggleBody = CommandShortcutEvent(
   key: 'toggle Body',
-  getDescription: () => AppFlowyEditorL10n.current.cmdToggleCode,
+  getDescription: () => AppFlowyEditorL10n.current.cmdToggleBody,
   command: 'ctrl+shift+b',
   macOSCommand: 'cmd+shift+b',
   handler: (editorState) => _toggleAttribute(editorState, 1, true),
