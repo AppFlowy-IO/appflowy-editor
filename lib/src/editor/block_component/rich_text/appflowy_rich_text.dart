aabdfca8e09080b87366ad0fb952dacc8ec366f8
Lucas.Xu <lucas.xu@appflowy.io>
2025-03-14 11:40:55 +0800

    feat: add text span overlay

--- lib/src/editor/block_component/rich_text/appflowy_rich_text.dart
+++ lib/src/editor/block_component/rich_text/appflowy_rich_text.dart
@@ -121,6 +121,7 @@ class _AppFlowyRichTextState extends State<AppFlowyRichText>
       children: [
         _buildPlaceholderText(context),
         _buildRichText(context),
+        ..._buildRichTextOverlay(context),
       ],
     );
 
@@ -408,6 +409,53 @@ class _AppFlowyRichTextState extends State<AppFlowyRichText>
       textScaler:
           TextScaler.linear(widget.editorState.editorStyle.textScaleFactor),
     );
+  }
+
+  List<Widget> _buildRichTextOverlay(BuildContext context) {
+    final widgets = <Widget>[];
+    final textInserts = widget.node.delta!.whereType<TextInsert>();
+    TextSpan textSpan = getTextSpan(textInserts: textInserts);
+    if (widget.textSpanDecorator != null) {
+      textSpan = widget.textSpanDecorator!(textSpan);
+    }
+    textSpan = adjustTextSpan(textSpan);
+    int index = 0;
+    for (final textInsert in textInserts) {
+      final rects = getRectsInSelection(
+        Selection(
+          start: Position(path: widget.node.path, offset: index),
+          end: Position(
+            path: widget.node.path,
+            offset: index + textInsert.length,
+          ),
+        ),
+      );
+      if (rects.isNotEmpty && textInsert.attributes?.href != null) {
+        widgets.add(
+          Positioned(
+            left: rects.first.left,
+            top: rects.first.top,
+            child: HoverMenu(
+              child: Container(
+                color: Colors.red.withValues(alpha: 0.5),
+                width: rects.first.width,
+                height: rects.first.height,
+              ),
+              itemBuilder: (context) => Material(
+                color: Colors.blue,
+                child: SizedBox(
+                  width: 200,
+                  height: 100,
+                  child: Text('This is a hover menu'),
+                ),
+              ),
+            ),
+          ),
+        );
+      }
+      index += textInsert.length;
+    }
+    return widgets;
   }
 
   Widget _buildAutoCompleteRichText() {
@@ -704,4 +752,68 @@ extension AppFlowyRichTextAttributes on Attributes {
   bool get autoComplete => this[AppFlowyRichTextKeys.autoComplete] == true;
 
   bool get transparent => this[AppFlowyRichTextKeys.transparent] == true;
+}
+
+class HoverMenu extends StatefulWidget {
+  final Widget child;
+  final WidgetBuilder itemBuilder;
+
+  const HoverMenu({
+    super.key,
+    required this.child,
+    required this.itemBuilder,
+  });
+
+  @override
+  HoverMenuState createState() => HoverMenuState();
+}
+
+class HoverMenuState extends State<HoverMenu> {
+  OverlayEntry? overlayEntry;
+
+  bool canCancelHover = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.text,
+      hitTestBehavior: HitTestBehavior.opaque,
+      onEnter: (details) {
+        overlayEntry = _createOverlayEntry();
+        Overlay.of(context).insert(overlayEntry!);
+      },
+      onExit: (details) {
+        if (canCancelHover) {
+          overlayEntry?.remove();
+          canCancelHover = false;
+        }
+      },
+      child: widget.child,
+    );
+  }
+
+  OverlayEntry _createOverlayEntry() {
+    final renderBox = context.findRenderObject() as RenderBox;
+    final offset = renderBox.localToGlobal(Offset.zero);
+    final size = renderBox.size;
+
+    return OverlayEntry(
+      maintainState: true,
+      builder: (context) => Positioned(
+        left: offset.dx,
+        top: offset.dy + size.height,
+        child: MouseRegion(
+          cursor: SystemMouseCursors.text,
+          hitTestBehavior: HitTestBehavior.opaque,
+          onEnter: (details) {
+            canCancelHover = true;
+          },
+          onExit: (details) {
+            canCancelHover = false;
+          },
+          child: widget.itemBuilder(context),
+        ),
+      ),
+    );
+  }
 }
