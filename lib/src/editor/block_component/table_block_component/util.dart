f0950f6130f133ae674f746b539dd9c9e97e7554
Lucas <lucas.xu@appflowy.io>
2025-03-19 10:21:01 +0800

    fix: parse table width/height error if the value is int (#1072)

--- lib/src/editor/block_component/table_block_component/util.dart
+++ lib/src/editor/block_component/table_block_component/util.dart
@@ -8,3 +8,35 @@ Node? getCellNode(Node tableNode, int col, int row) {
         n.attributes[TableCellBlockKeys.rowPosition] == row,
   );
 }
+
+extension TableCellNodeDynamicExtension on dynamic {
+  double toDouble({double defaultValue = 0.0}) {
+    if (this is int) {
+      return this.toDouble();
+    } else if (this is double) {
+      return this;
+    } else {
+      return double.tryParse(toString()) ?? defaultValue;
+    }
+  }
+}
+
+extension TableCellNodeAttributesExtension on Node {
+  double get cellWidth {
+    assert(type == TableCellBlockKeys.type);
+    return attributes[TableCellBlockKeys.width]?.toDouble() ??
+        TableDefaults.colWidth;
+  }
+
+  double get cellHeight {
+    assert(type == TableCellBlockKeys.type);
+    return attributes[TableCellBlockKeys.height]?.toDouble() ??
+        TableDefaults.rowHeight;
+  }
+
+  double get colHeight {
+    assert(type == TableBlockKeys.type);
+    return attributes[TableBlockKeys.colsHeight]?.toDouble() ??
+        TableDefaults.rowHeight;
+  }
+}
