9e413d3dde40059b31902eb92f1c8f3e952784ee
Sean Riley Hawkins <rileyhawk249@gmail.com>
2025-03-31 21:29:03 +0200

    refactor: use a vim state class to manage the buffer
    - By using the vim state class we can ensure that no key sequence is
    missed by both the user and flutter tests.
    - Also makes it easier to add/remove key handlers.

--- lib/src/editor/editor_component/service/shortcuts/vim/vim_fsm.dart
+++ lib/src/editor/editor_component/service/shortcuts/vim/vim_fsm.dart
@@ -7,49 +7,33 @@ import 'package:flutter/services.dart';
 import 'package:appflowy_editor/src/editor/command/selection_commands.dart';
 import './vim_cursor.dart';
 
-const baseKeys = ['h', 'j', 'k', 'l', 'i'];
+const baseKeys = ['h', 'j', 'k', 'l', 'i', 'a', 'o', 'w'];
 
-String _buffer = '';
-String _deleteBuffer = '';
-
-CommandShortcutEventHandler vimMoveCursorToStartHandler = (editorState) {
-  if (!editorState.vimMode) {
-    return KeyEventResult.ignored;
-  }
+void vimMoveCursorToStartHandler(EditorState editorState) {
+  if (!editorState.vimMode) {}
   if (editorState.mode == VimModes.normalMode) {
     final selection = editorState.selection;
-    if (selection == null) {
-      return KeyEventResult.ignored;
-    }
+    if (selection == null) {}
     if (isRTL(editorState)) {
       editorState.moveCursorBackward(SelectionMoveRange.line);
     } else {
       editorState.moveCursorForward(SelectionMoveRange.line);
     }
-    return KeyEventResult.handled;
   }
-  return KeyEventResult.ignored;
-};
+}
 
-CommandShortcutEventHandler vimMoveCursorToEndHandler = (editorState) {
-  if (!editorState.vimMode) {
-    return KeyEventResult.ignored;
-  }
+void vimMoveCursorToEndHandler(EditorState editorState) {
+  if (!editorState.vimMode) {}
   if (editorState.mode == VimModes.normalMode) {
     final selection = editorState.selection;
-    if (selection == null) {
-      return KeyEventResult.ignored;
-    }
+    if (selection == null) {}
     if (isRTL(editorState)) {
       editorState.moveCursorForward(SelectionMoveRange.line);
     } else {
       editorState.moveCursorBackward(SelectionMoveRange.line);
     }
-
-    return KeyEventResult.handled;
   }
-  return KeyEventResult.ignored;
-};
+}
 
 Position deleteCurrentLine(EditorState editorState, int count) {
   final selection = editorState.selection;
@@ -57,9 +41,7 @@ Position deleteCurrentLine(EditorState editorState, int count) {
   final node = editorState.getNodeAtPath(selection.start.path);
   if (node == null || node.delta == null) return Position(path: [0], offset: 0);
   final startPath = selection.start.path.first;
-  //print('start delete point: $startPath');
   final endPath = startPath + count - 1;
-  //print('final end point: $endPath');
   final tmpPosition = Position(path: [startPath], offset: 0);
   final delta = node.delta;
   for (int i = startPath; i <= endPath; i++) {
@@ -69,9 +51,7 @@ Position deleteCurrentLine(EditorState editorState, int count) {
           end: Position(path: selection.end.path, offset: delta.length));
       editorState.selection = deletionSelection;
       final transaction = editorState.transaction;
-      //transaction.deleteText(node, 0, node.delta!.length);
       transaction.deleteNodesAtPath(editorState.selection!.start.path);
-      // transaction.deleteNode(node);
 
       editorState
           .apply(transaction)
@@ -85,28 +65,37 @@ Position deleteCurrentLine(EditorState editorState, int count) {
   return tmpPosition;
 }
 
-String keyBuffer = '';
-Timer? resetTimer;
+class VimState {
+  String commandBuffer = '';
+  String deleteBuffer = '';
+  Timer? resetTimer;
+  void reset() {
+    commandBuffer = '';
+    deleteBuffer = '';
+    resetTimer?.cancel();
+  }
 
-String deleteBuffer = '';
+  void scheduleReset() {
+    resetTimer?.cancel();
+    resetTimer = Timer(const Duration(milliseconds: 500), reset);
+  }
+}
 
 class VimFSM {
+  static final VimFSM _instance = VimFSM._internal();
+  factory VimFSM() => _instance;
+  VimFSM._internal();
+  final _state = VimState();
+
   KeyEventResult processKey(KeyEvent event, EditorState editorState) {
     if (event is! KeyDownEvent) {
       return KeyEventResult.ignored;
     }
+    _state.scheduleReset();
 
-    resetTimer?.cancel();
-    resetTimer = Timer(Duration(milliseconds: 500), () {
-      keyBuffer = "";
-      _buffer = "";
-    });
     final key = event.logicalKey.keyLabel.toLowerCase();
     if (baseKeys.contains(key)) {
-      //print('buffer key readout: $_buffer');
-      final count = _buffer.isNotEmpty ? int.parse(_buffer) : 1;
-      //print('counter for debugger: $count');
-      resetBuffer();
+      final count = _parseCount(_state.commandBuffer);
       final selection = editorState.selection;
       if (selection == null) return KeyEventResult.ignored;
 
@@ -118,77 +107,55 @@ class VimFSM {
           Selection.collapsed(newPosition),
           reason: SelectionUpdateReason.uiEvent,
         );
-        resetBuffer();
         return KeyEventResult.handled;
       }
-    } else {
-      //BUG: _buffer is causing double keys like 440 instead of 40
-      _buffer += key;
-      deleteBuffer += key;
-      //NOTE: Resetting the _buffer here will break line jumping
-      // resetBuffer();
-      resetSequenceBuffer();
-
-      keyBuffer += key;
-      /*print(
-          'After appending, keyBuffer="$keyBuffer", _buffer="$_buffer", deleteBuffer="$deleteBuffer"');
-          */
-      if (RegExp(r'^\d$').hasMatch(key)) {
-        if (_buffer == 'shift left4' ||
-            _buffer == 'shift right4' ||
-            event.character == '\$') {
-          vimMoveCursorToEndHandler(editorState);
-          resetSequenceBuffer();
-          resetBuffer();
-        }
-
-        if (keyBuffer == '0' && _buffer == '0') {
-          vimMoveCursorToStartHandler(editorState);
-          resetBuffer();
-          resetSequenceBuffer();
-        }
-        return KeyEventResult.handled;
-      }
-
-      if (deleteBuffer.endsWith('dd')) {
-        // final tmpString = deleteBuffer + keyBuffer;
-        final RegExp ddRegExp = RegExp(r'^(\d+)?dd$');
-        final match = ddRegExp.firstMatch(deleteBuffer);
-        if (match != null) {
-          final String? countStr = match.group(1);
-          final int count = (countStr != null && countStr.isNotEmpty)
-              ? int.parse(countStr)
-              : 1;
-          final tmpPosition = deleteCurrentLine(editorState, count);
-          editorState.selection = Selection(
-            end: tmpPosition,
-            start: tmpPosition,
-          );
-          deleteBuffer = '';
-          keyBuffer = '';
-          // _buffer = '';
-          return KeyEventResult.handled;
-        }
-      }
-
-      deleteBuffer = '';
-      resetBuffer();
-
+    }
+    String keySequence = _buildKeySequence(event);
+    _state.commandBuffer += keySequence;
+    if (_handleSpecialCommands(editorState)) {
       return KeyEventResult.handled;
     }
 
-    resetSequenceBuffer();
-    resetBuffer();
-    deleteBuffer = '';
     return KeyEventResult.ignored;
   }
 
-  void resetBuffer() {
-    _buffer = '';
+  int _parseCount(String buffer) {
+    final match = RegExp(r'^(\d+)').firstMatch(buffer);
+    if (match != null) {
+      return int.tryParse(match.group(1) ?? '') ?? 1;
+    }
+    return 1;
   }
 
-  void resetSequenceBuffer() {
-    keyBuffer = "";
-    resetTimer?.cancel();
+  String _buildKeySequence(KeyEvent event) {
+    final key = event.logicalKey.keyLabel.toLowerCase();
+    if (key.startsWith('shift')) {
+      return key;
+    }
+    return key;
+  }
+
+//TODO: Handle other keys from vim.dart
+  bool _handleSpecialCommands(EditorState editorState) {
+    final buffer = _state.commandBuffer;
+    if (buffer == 'shift left4' || buffer == 'shift right4') {
+      vimMoveCursorToEndHandler(editorState);
+      _state.reset();
+      return true;
+    }
+
+    if (buffer == '0') {
+      vimMoveCursorToStartHandler(editorState);
+      _state.reset();
+      return true;
+    }
+    if (buffer.endsWith('dd')) {
+      final count = _parseCount(buffer.substring(0, buffer.length - 2));
+      final position = deleteCurrentLine(editorState, count);
+      editorState.selection = Selection(start: position, end: position);
+      _state.reset();
+      return true;
+    }
+    return false;
   }
 }
