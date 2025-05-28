8134f34cfad71f263aab00f4354cc5dd8a0a6433
Peng Zhou <penkzhou@gmail.com>
2025-05-28 20:45:31 +0800

    chore: update localization strings and test data
    - Simplified localization strings for H1, H2, and H3 commands in l10n.dart.
    - Updated documentSample in quill_delta_encoder_test.dart to reflect changes in data structure.
    - No changes to pubspec.yaml this time, just keeping the localization and test data in sync.

--- lib/src/l10n/l10n.dart
+++ lib/src/l10n/l10n.dart
@@ -1844,7 +1844,7 @@ class AppFlowyEditorLocalizations {
   /// `toggle H1`
   String get cmdToggleH1 {
     return Intl.message(
-      'toggle into Heading 1',
+      'toggle H1',
       name: 'cmdToggleH1',
       desc: '',
       args: [],
@@ -1854,7 +1854,7 @@ class AppFlowyEditorLocalizations {
   /// `toggle H2`
   String get cmdToggleH2 {
     return Intl.message(
-      'toggle into Heading 2',
+      'toggle H2',
       name: 'cmdToggleH2',
       desc: '',
       args: [],
@@ -1864,7 +1864,7 @@ class AppFlowyEditorLocalizations {
   /// `toggle H3`
   String get cmdToggleH3 {
     return Intl.message(
-      'toggle into Heading 3',
+      'toggle H3',
       name: 'cmdToggleH3',
       desc: '',
       args: [],
