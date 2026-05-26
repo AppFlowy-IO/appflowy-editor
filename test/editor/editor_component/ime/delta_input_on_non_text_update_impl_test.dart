72ce7cc150f8de009dba065d9c8e2749532d108c
Lucas <lucas.xu@appflowy.io>
2026-05-26 11:51:13 +0800

    chore: optimize gboard logic (#1207)

--- test/editor/editor_component/ime/delta_input_on_non_text_update_impl_test.dart
+++ test/editor/editor_component/ime/delta_input_on_non_text_update_impl_test.dart
@@ -3,6 +3,8 @@ import 'package:appflowy_editor/src/editor/editor_component/service/ime/delta_in
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../new/infra/testable_editor.dart';
+
 void main() {
   group('onNonTextUpdate', () {
     // Pro-performa test
@@ -16,6 +18,44 @@ void main() {
         EditorState.blank(),
         [],
       );
+    });
+
+    testWidgets('handles Android IME select all as document select all',
+        (tester) async {
+      const text = 'AppFlowy';
+      final editor = tester.editor..addParagraphs(3, initialText: text);
+      await editor.startTesting();
+      await editor.updateSelection(
+        Selection.collapsed(Position(path: [1], offset: 3)),
+      );
+      final oldText = List.filled(3, text).join('\n');
+
+      final handled = await handleAndroidNonTextUpdate(
+        TextEditingDeltaNonTextUpdate(
+          oldText: oldText,
+          selection: TextSelection(
+            baseOffset: 0,
+            extentOffset: oldText.length,
+          ),
+          composing: TextRange.empty,
+        ),
+        editor.editorState,
+      );
+
+      expect(handled, true);
+      expect(
+        editor.selection,
+        Selection(
+          start: Position(path: [0]),
+          end: Position(path: [2], offset: text.length),
+        ),
+      );
+      expect(
+        editor.editorState.selectionUpdateReason,
+        SelectionUpdateReason.selectAll,
+      );
+
+      await editor.dispose();
     });
   });
 }
