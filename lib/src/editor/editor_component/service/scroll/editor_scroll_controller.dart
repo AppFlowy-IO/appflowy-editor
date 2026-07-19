9dd4c6d8d4f74e6426e8474acd9297a24b9abfdf
Nathan <nathan@appflowy.io>
2026-07-19 14:25:09 +0800

    fix: detach shrink-wrapped editor listeners on dispose

--- lib/src/editor/editor_component/service/scroll/editor_scroll_controller.dart
+++ lib/src/editor/editor_component/service/scroll/editor_scroll_controller.dart
@@ -24,22 +24,13 @@ class EditorScrollController {
     // if shrinkWrap is true, we will render the document with Column layout.
     // otherwise, we will render the document with ScrollablePositionedList.
     if (shrinkWrap) {
-      void updateVisibleRange() {
-        visibleRangeNotifier.value = (
-          0,
-          editorState.document.root.children.length - 1,
-        );
-      }
-
-      updateVisibleRange();
-      editorState.document.root.addListener(updateVisibleRange);
+      _updateVisibleRange();
+      editorState.document.root.addListener(_updateVisibleRange);
 
       shouldDisposeScrollController = scrollController == null;
       this.scrollController = scrollController ?? ScrollController();
       // listen to the scroll offset
-      this.scrollController.addListener(
-            () => offsetNotifier.value = this.scrollController.offset,
-          );
+      this.scrollController.addListener(_updateScrollOffset);
     } else {
       // listen to the scroll offset
       _scrollOffsetSubscription = _scrollOffsetListener.changes.listen((value) {
@@ -137,18 +128,32 @@ class EditorScrollController {
 
   late final StreamSubscription<double> _scrollOffsetSubscription;
 
+  void _updateVisibleRange() {
+    visibleRangeNotifier.value = (
+      0,
+      editorState.document.root.children.length - 1,
+    );
+  }
+
+  void _updateScrollOffset() {
+    offsetNotifier.value = scrollController.offset;
+  }
+
   // dispose the subscription
   void dispose() {
-    if (shouldDisposeScrollController) {
-      scrollController.dispose();
-    }
-
-    if (!shrinkWrap) {
+    if (shrinkWrap) {
+      editorState.document.root.removeListener(_updateVisibleRange);
+      scrollController.removeListener(_updateScrollOffset);
+    } else {
       _scrollOffsetSubscription.cancel();
       _itemPositionsListener.itemPositions.removeListener(_listenItemPositions);
       (_itemPositionsListener as ItemPositionsNotifier?)
           ?.itemPositions
           .dispose();
+    }
+
+    if (shouldDisposeScrollController) {
+      scrollController.dispose();
     }
 
     offsetNotifier.dispose();
